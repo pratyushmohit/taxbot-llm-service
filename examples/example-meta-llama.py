9b14575f52ee160748af241eae9b32c275fb7854
Pratyush Mohit <pratyushmohit226@gmail.com>
2025-01-15 14:43:47 +0530

    Delete examples directory

--- examples/example-meta-llama.py
+++
@@ -1,5 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3.1-8B", max_new_tokens=512)
-pipe("Hey how are you doing today?")
